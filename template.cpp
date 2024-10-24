25de575a7f224eafe0a9daf611f8b0defd0e2088
Simon <simonwa01@gmail.com>
2024-10-24 23:26:12 +0200

    ✨ NEW: template functions

--- template.cpp
+++ template.cpp
@@ -45,6 +45,7 @@ using namespace std;
 #define sl set<long long>
   
 /* 2. FUNCTIONS */
+#define strl_read(...) string __VA_ARGS__; getline(cin, __VA_ARGS__)
 #define ll_read(...) ll __VA_ARGS__; read(__VA_ARGS__)
 #define str_read(...) string __VA_ARGS__; read(__VA_ARGS__)
 #define dbl_read(...) double __VA_ARGS__; read(__VA_ARGS__)
@@ -156,8 +157,10 @@ ll max(ll a,int b) { if (a>b) return a; return b; }
 ll max(int a,ll b) { if (a>b) return a; return b; }
 ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
 ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
-string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
-string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+char to_upper(char a) {if (a >= 'a' && a <= 'z')  a -= 'a' - 'A';return a;}
+char to_lower(char a) { if (a >= 'A' && a <= 'Z') a += 'a' - 'A';return a;}
+string to_upper(string a) {for (int i = 0; i < (int)a.size(); ++i) a[i] = to_upper(a[i]); return a;}
+string to_lower(string a) {for (int i = 0; i < (int)a.size(); ++i) a[i] = to_lower(a[i]); return a;}
 bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
 void yes() { cout<<"YES\n"; }
 void no() { cout<<"NO\n"; }
